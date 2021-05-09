dbe8a84524b0d5634c7f7d0876c5908a89b199b5
Sampriti <sampritipatel@gmail.com>
2021-05-09 12:44:41 +0530

    final

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.login, name="loginL"),
-    path('login/home', views.index, name = "home"),
+    path('home', views.index, name = "home"),
     path('need', views.need , name= "need"),
     path('give', views.give, name= "give"),
     path('need/add_request', views.add_request, name= "add_request"),
